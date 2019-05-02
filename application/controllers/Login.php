a8623de2d44dc7dbf8af4f2acb932af551c83bac
zubair042 <042zubair@github.com>
2019-05-02 12:40:20 +0500

    commit

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,7 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-
+	public function __construct(){
+		parent::__construct();
+	}
 	public function index()
 	{
 		$this->load->view('login/index');
