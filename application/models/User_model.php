659f34dac37e1dcdcba41095f084c2c706187d1e
zubair042 <042zubair@github.com>
2019-05-06 18:12:21 +0500

    commit

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -24,6 +24,25 @@
 				return false;
 			}
 		}
+
+		public function add_new_user($post){
+			$res = 'success';
+			$this->load->model('common_model');	
+			$checkUserExists = $this->common_model->listingRow('email',$post['email'],"user");
+			if(count($checkUserExists)>0){
+				$res = 'email_exists';
+				echo $res;
+				return false;
+			}
+			$data['company_id'] = $post['company_id'];
+			$data['full_name'] = $post['full_name'];
+			$data['email'] = $post['email'];
+			$data['password'] = md5($post['password']);
+			$data['created_at'] = date('Y-m-d H:i:s');
+			$result = $this->db->insert("user",$data);
+			echo $res;
+			return $result;
+		}
 	}	
 	
 ?>
